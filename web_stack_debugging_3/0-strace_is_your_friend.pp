229c1f3d7bb3a5fd051da469e44452491306bd8f
oreste-abizera <oresteabizera11@gmail.com>
2024-07-30 11:20:27 +0200

    chore: fix typo in wp-settings.php file

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix apache and automate it using Puppet
+exec { 'fixing typo...':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
